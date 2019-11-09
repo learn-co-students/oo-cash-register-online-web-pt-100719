3837e98eca461b6db2fcde0f4835016d5be4ef85
Derric Fragosa <fderric@gmail.com>
2019-11-09 16:51:10 +0000

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,12 +1,24 @@
 
 class CashRegister
   
-  attr_reader :total 
+  attr_accessor :total 
+  attr_reader :discount
   
-  def initialize
+  def initialize(discount = 0)
     @total = 0 
+    discount = discount
+  end 
+  
+  def total
+    return @total
   end 
 
-
+  def discount
+    @discount = 0.2
+  end 
+  
+  def add_item(title, price, quantity = 1)
+    @total = @total + price * quantity
+  end 
 
 end 
